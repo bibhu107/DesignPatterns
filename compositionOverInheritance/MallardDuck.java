dcff9f9d1ff28ae71c2ca62f1f6570b4b65502e1
bibhu <prasadpal107@gmail.com>
2020-04-05 19:56:04 +0530

    Intial Changes for CompositeOverInheritance

---
+++ compositionOverInheritance/MallardDuck.java
@@ -0,0 +1,9 @@
+public class MallardDuck {
+    int x = 5;
+
+    public static void main(String[] args) {
+        MallardDuck myObj = new MallardDuck();
+        System.out.println(myObj.x);
+    }
+}
+
