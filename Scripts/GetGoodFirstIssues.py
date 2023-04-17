19f44919585eb11d5c337c3d5e3030aa34aa0593
bibhu <prasadpal107@gmail.com>
2023-04-17 16:27:07 +0530

    fix

--- Scripts/GetGoodFirstIssues.py
+++ Scripts/GetGoodFirstIssues.py
@@ -1,8 +1,7 @@
 # If you are a new to open source and want to try easy issues first then this script can be useful
 # To run this script you must have GithubPersonalToken
-# Sample running command 
 #Install pip install PyGithub
-
+# Sample running command 
 #  python3 GetGoodFirstIssues.py --period 10 --stars 2000 --language java
 
 import argparse
@@ -16,8 +15,8 @@ parser.add_argument('--period', type=int, default=180,
                     help='Filter issues by age in days (default: 180)')
 parser.add_argument('--stars', type=int, default=2000,
                     help='Filter repositories by stars (default: 2000)')
-parser.add_argument('--language', type=str, default='java',
-                    help='Filter repositories by programming language (default: java)')
+parser.add_argument('--language', type=str, default='',
+                    help='Filter repositories by programming language (default: None)')
 args = parser.parse_args()
 
 # Access the Github API with personal access token
@@ -27,8 +26,11 @@ try:
 except:
     print("Error: Unable to access Github API")
 
-# Search for repositories with the specified language and stars > args.stars
-query = f"language:{args.language} stars:>{args.stars}"
+# Search for repositories with the specified language (if provided) and stars > args.stars
+if args.language:
+    query = f"language:{args.language} stars:>{args.stars}"
+else:
+    query = f"stars:>{args.stars}"
 results = g.search_repositories(query)
 
 # Print the list of good first issues in each repository, sorted by creation date and filtered by age
@@ -40,3 +42,4 @@ for repo in results:
     new_issues = [issue for issue in issues if issue.created_at > period_ago and (not issue.assignee or (issue.state == 'open' and issue.updated_at < period_ago - timedelta(days=14)))]    
     for issue in new_issues:
         print(f"{repo.name}: {issue.title} - {issue.html_url}")
+
