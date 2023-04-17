055a685fb2390c2d0ec3c1922702b36c30d65013
bibhu <prasadpal107@gmail.com>
2023-04-17 15:16:03 +0530

    modified github good first issue script

--- Scripts/GetGoodFirstIssues.py
+++ Scripts/GetGoodFirstIssues.py
@@ -1,8 +1,10 @@
+from datetime import datetime, timedelta
 from github import Github
 
 # Access the Github API with personal access token
+github_personal_token = input("Enter github personal token: ")
 try:
-    g = Github("<personal_access_token>")
+    g = Github(github_personal_token)
 except:
     print("Error: Unable to access Github API")
 
@@ -10,8 +12,11 @@ except:
 query = "language:java stars:>2000"
 results = g.search_repositories(query)
 
-# Print the list of good first issues in each repository
+# Print the list of good first issues in each repository, sorted by creation date and filtered by age
 for repo in results:
     issues = repo.get_issues(labels=["good first issue"])
-    for issue in issues:
+    issues = sorted(issues, key=lambda issue: issue.created_at)
+    six_months_ago = datetime.now() - timedelta(days=180)
+    new_issues = [issue for issue in issues if issue.created_at > six_months_ago]
+    for issue in new_issues:
         print(f"{repo.name}: {issue.title} - {issue.html_url}")
